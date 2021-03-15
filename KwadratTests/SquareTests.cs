3457cf3c04155092f715d14c34056129bfc02047
Mateusz Kaczmarkiewicz <matt327k@gmail.com>
2021-03-15 10:09:47 +0100

    pierwsze zadanie

---
+++ KwadratTests/SquareTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kwadrat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kwadrat.Tests
+{
+    [TestClass()]
+    public class SquareTests
+    {
+        [TestMethod()]
+        public void setSideTest()
+        {
+            Square mySquare = new Square(2);
+
+            double input = 4;
+
+            mySquare.setSide(input);
+
+            Assert.AreEqual(mySquare.getSide(), input);
+        }
+
+        [TestMethod()]
+        public void getSideTest()
+        {
+            double input = 4;
+
+            Square mySquare = new Square(input);
+
+            Assert.AreEqual(mySquare.getSide(), input);
+        }
+
+        [TestMethod()]
+        public void getAreaTest()
+        {
+            double input = 4;
+
+            Square mySquare = new Square(input);
+
+            Assert.AreEqual(mySquare.getArea(), input * input);
+        }
+    }
+}
